94fd82fb5932a41f378c17ef97a21f20026f5a90
Branden-Kunkel <kunkel.branden6130@gmail.com>
2022-10-30 15:36:13 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="Twitter Aggregate Generator",
+        version="1.0.1",
+        description="A program to collect and aggregate large amounts of twitter data via the Twitter API.",
+        author="Branden Kunkel",
+        author_email="kunkel.branden6130@gmail.com",
+        maintainer="Branden Kunkel",
+        maintainer_email="kunkel.branden6130@gmail.com",
+        url="https://github.com/Branden-Kunkel/twitter_aggregate_generator",
+        python_requires="==3.8.10",
+        install_requires=["requests == 2.28.1"],
+)
